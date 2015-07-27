c365dde63a20d76cc4599931949a5cf7f45f8477
dumky <julien.couvreur@gmail.com>
2015-07-26 17:00:06 -0700

    Added all three templates placeholders

---
+++ content/2015/07/new.md
@@ -0,0 +1,5 @@
+This is a new blog post.
+
+<a href="http://google.com">Google</a>
+
+[Google](http://google.com)
--- src/BlogBuilder/Program.cs
+++ src/BlogBuilder/Program.cs
@@ -15,22 +15,40 @@ namespace BlogBuilder
         public static string contentRoot = @"C:\Users\Julien\Documents\GitHubVisualStudio\blog\content\";
         public static string templateRoot = @"C:\Users\Julien\Documents\GitHubVisualStudio\blog\templates\";
         public static string staticRoot = @"C:\Users\Julien\Documents\GitHubVisualStudio\blog\static\";
+        public static string outputRoot = @"C:\Users\Julien\Documents\GitHubVisualStudio\blog\output\";
 
         public static string FrontPageTemplate
         {
             get
             {
-                // TODO: add caching
                 return File.ReadAllText(Path.Combine(templateRoot, "index.template"));
+            }
+        }
+        public static string EntryTemplate
+        {
+            get
+            {
+                return File.ReadAllText(Path.Combine(templateRoot, "entry.template"));
+            }
+        }
+
+        public static string RSSTemplate
+        {
+            get
+            {
+                return File.ReadAllText(Path.Combine(templateRoot, "rss.template"));
             }
         }
 
         static void Main(string[] args)
         {
+            Template.NamingConvention = new DotLiquid.NamingConventions.CSharpNamingConvention();
 
             var index = LoadIndex();
 
             index.OutputEntries();
+            index.OutputFrontPage();
+            index.OutputRSS();
         }
 
         private static Index LoadIndex()
@@ -44,7 +62,7 @@ namespace BlogBuilder
         }
     }
 
-    public class Index
+    public class Index : Drop
     {
         [YamlMember(Alias = "blog-title")]
         public string BlogTitle { get; set; }
@@ -52,9 +70,16 @@ namespace BlogBuilder
         [YamlMember(Alias = "blog-author")]
         public string BlogAuthor { get; set; }
 
+        [YamlMember(Alias = "blog-contact")]
+        public string BlogContact { get; set; }
+
         [YamlMember(Alias = "blog-url")]
         public string BlogUrl { get; set; }
 
+        [YamlMember(Alias = "blog-description")]
+        public string BlogDescription { get; set; }
+
+        // TODO: sort entries when set
         public List<Entry> Entries
         {
             get; set;
@@ -62,23 +87,55 @@ namespace BlogBuilder
 
         public void OutputEntries()
         {
-            Template template = Template.Parse(Program.FrontPageTemplate);
-            var output = template.Render(Hash.FromAnonymousObject(new { name = "tobi" }));
+            Template template = Template.Parse(Program.EntryTemplate);
 
+            foreach (var entry in Entries)
+            {
+                // TODO: check if content is newer and output should be deleted
+                var output = template.Render(Hash.FromAnonymousObject(new { current = entry, index = this }));
+
+                EnsureDirectoryExists(entry.OutputFullPath);
+                File.WriteAllText(entry.OutputFullPath, output);
+            }
         }
 
         public void OutputFrontPage()
         {
+            Template template = Template.Parse(Program.FrontPageTemplate);
+            var output = template.Render(Hash.FromAnonymousObject(new { index = this }));
+            File.WriteAllText(FrontPageFullPath, output);
+        }
 
+        public string FrontPageFullPath
+        {
+            get
+            {
+                return Path.Combine(Program.outputRoot, "index.html");
+            }
         }
 
         public void OutputRSS()
         {
+            Template template = Template.Parse(Program.RSSTemplate);
+            var output = template.Render(Hash.FromAnonymousObject(new { index = this }));
+            File.WriteAllText(RSSFullPath, output);
+        }
 
+        public string RSSFullPath
+        {
+            get
+            {
+                return Path.Combine(Program.outputRoot, "index.rss");
+            }
+        }
+
+        public void EnsureDirectoryExists(string path)
+        {
+            new System.IO.FileInfo(path).Directory.Create();
         }
     }
 
-    public class Entry
+    public class Entry : Drop
     {
         public string Title
         {
@@ -114,6 +171,22 @@ namespace BlogBuilder
                 md.SafeMode = false;
                 string output = md.Transform(MD);
                 return output;
+            }
+        }
+
+        public string WebRelativePath
+        {
+            get
+            {
+                return Path.Combine(@"archive\", Source).Replace(".md", ".html");
+            }
+        }
+
+        public string OutputFullPath
+        {
+            get
+            {
+                return Path.Combine(Program.outputRoot, @"archive\", Source).Replace(".md", ".html");
             }
         }
 
